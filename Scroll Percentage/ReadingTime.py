377f692b136b76e8c5a3f2c2e10991477d8feb4a
vikas garg <33121984+vikasgargvi@users.noreply.github.com>
2018-10-15 22:36:32 +0530

    Create ReadingTime.py

---
+++ Scroll Percentage/ReadingTime.py
@@ -0,0 +1,22 @@
+import math
+
+class CalculateTime():
+
+	def calTime(text):
+		length = len(text.split())
+		time = length/200
+		minutes = math.floor(time)
+		seconds = (time-minutes)*60
+		seconds = math.ceil(seconds) if math.ceil(seconds)-seconds < 0.5 else math.floor(seconds)
+		return str(minutes) + " minutes " + str(seconds) + " seconds "
+    
+    
+**ALSO MAKE ENTRIES IN**
+
+**models.py --> class Post**
+avg_time = models.CharField(max_length = 40, default='')
+
+**views.py --> class in which ypu store post in database**
+  //add before saving post in database
+    post.avg_time = CalculateTime.calTime(post.text)
+    
