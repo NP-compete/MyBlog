08e7a79f510ae9a1370dc320c5ef8292c068cb94
vikas garg <33121984+vikasgargvi@users.noreply.github.com>
2018-10-15 23:46:21 +0530

    Update ReadingTime.py

--- Scroll Percentage/ReadingTime.py
+++ Scroll Percentage/ReadingTime.py
@@ -10,13 +10,17 @@ class CalculateTime():
 		seconds = math.ceil(seconds) if math.ceil(seconds)-seconds < 0.5 else math.floor(seconds)
 		return str(minutes) + " minutes " + str(seconds) + " seconds "
     
-    
+'''    
 # ALSO MAKE ENTRIES IN
+
+Place this file in the same directory where is views.py
 
 ** models.py --> class Post
 avg_time = models.CharField(max_length = 40, default='')
 
 ** views.py --> class in which you store post in database
+// import
+	from .ReadingTime import calculateTime
   //add before saving post in database
     post.avg_time = CalculateTime.calTime(post.text)
-    
+'''
